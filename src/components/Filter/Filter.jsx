17fd3d45f8a034a3f603d13127934bd3ba7f64db
Andriyan78 <110392316+Andriyan78@users.noreply.github.com>
2023-03-18 14:35:09 +0100

    phone-done

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,21 @@
+import { Label, Input } from './Filter.styled';
+import PropTypes from 'prop-types';
+
+export const Filter = ({ onChange }) => {
+  return (
+    <Label>
+      Find contacts by name
+      <Input
+        name="filter"
+        type="text"
+        
+        onChange={onChange}
+      ></Input>
+    </Label>
+  );
+};
+
+Filter.propTypes = {
+ 
+  onChange: PropTypes.func.isRequired,
+};
